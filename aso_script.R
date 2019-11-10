5660cb63c62c68cf88396dd328a33272537d6aca
Caroline Owens <carolinehowens@gmail.com>
2019-11-10 14:29:28 -0800

    Opening and reading ASO data

--- aso_script.R
+++ aso_script.R
@@ -1,1 +1,16 @@
 #script to import and work with TIFF data
+
+install.packages("raster")
+library(raster)
+install.packages("rgdal")
+library(rgdal)
+
+setwd("G:/Shared drives/PSTAT-Geospatial_Model_Selection")
+#I downloaded some of the ASO data into the google drive
+
+GDALinfo("135988930/ASO_3M_QF_USCOGM_20170221.tif")
+#this allows you to view the spatial metadata of the TIFF file
+
+dat1 <- raster("135988930/ASO_3M_QF_USCOGM_20170221.tif")
+#import the data as a raster layer
+
