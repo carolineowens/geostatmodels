39e306c48e4adcacc98c0e771c389020c99d6fc6
Caroline Owens <carolinehowens@gmail.com>
2019-11-12 16:10:14 -0800

    Tatum's changes 12 nov to create rasters from the new ASO data

--- aso_script.R
+++ aso_script.R
@@ -1,4 +1,5 @@
 #script to import and work with TIFF data
+# from https://www.earthdatascience.org/courses/earth-analytics/lidar-raster-data-r/introduction-to-spatial-metadata-r/
 
 install.packages("raster")
 library(raster)
@@ -14,3 +15,13 @@ GDALinfo("135988930/ASO_3M_QF_USCOGM_20170221.tif")
 dat1 <- raster("135988930/ASO_3M_QF_USCOGM_20170221.tif")
 #import the data as a raster layer
 
+crs(dat1)
+#view coordinate reference system of the data - here WGS84
+
+dat1@extent
+#view spatial extent via the slot (not sure what slot means)
+
+nlayers(dat1)
+#shows how many layers/bands the dataset has - here, 1
+
+plot(dat1) #visualize the layer
